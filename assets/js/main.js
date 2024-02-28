f13de08e52c05e6323b9d5ff647128a7e711b6e7
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-28 16:34:39 +0100

    add variables in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,18 @@
 // console.log('connesso');
 
+// creare delle variabili in js
+// una variabile per collegare il bottone 
+
+const btn = document.getElementById('btn');
+
+console.log(btn);
+// una variabile per il numero di celle che serviranno
+
+const numberOfCell = 100;
+
+console.log(numberOfCell);
+// una variabile con il markup delle singole celle
+
+const markupCell = `<div class="cell"></div>`;
+
+console.log(markupCell);
