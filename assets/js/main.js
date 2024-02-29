b5d2188f6db817bf47dd34c1d1569193186e39fc
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-29 15:27:44 +0100

    add eventlistener to single cell to change color and sent cellnumber to console with .toggle in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,6 @@
 // console.log('connesso');
 
 // creare delle variabili in js
-// una variabile per collegare il bottone e renderlo attivo per generare il gioco
-
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', function () {
-
-    for (let i = 1; i <= numberOfCell; i++) {
-
-    const numberInCell = i;
-   
-    const cellNode = document.createElement('div');
-
-    cellNode.classList.add('cell');
-
-    cellNode.textContent = numberInCell;
-
-    console.log(cellNode);
-
-    gridContainer.insertAdjacentElement('beforeend', cellNode);
-
-}
-})
-// console.log(btn);
 // una variabile per il numero di celle che serviranno
 
 const numberOfCell = 100;
@@ -35,6 +12,38 @@ const numberOfCell = 100;
 const gridContainer = document.querySelector('.container');
 
 console.log(gridContainer);
+
+// una variabile per collegare il bottone e renderlo attivo per generare il gioco
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function () {
+
+    for (let i = 1; i <= numberOfCell; i++) {
+
+        const numberInCell = i;
+        // creazione singola cella ed inserimento nel container
+        const cellNode = document.createElement('div');
+
+        cellNode.classList.add('cell');
+
+        cellNode.textContent = numberInCell;
+        // rendiamo le singole celle attive, quando si cliccano cambiano colore ed inviano il numero in console
+        cellNode.addEventListener('click', function () {
+            
+            cellNode.classList.toggle('bg_active-cell');
+            console.log(cellNode.innerText);
+        })
+
+        console.log(cellNode);
+
+        gridContainer.insertAdjacentElement('beforeend', cellNode);
+
+    }
+
+})
+
+
 
 // creare un ciclo (for) per generare il numero di celle con all'interno i numeri in progressione
 
